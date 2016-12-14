41c8f3381d5afca41c0b6ebe283050b1d949a1e6
Eren Tantekin <eren@eMBA.local>
2016-12-14 12:13:58 -0800

    checkin

---
+++ background.js
@@ -0,0 +1,17 @@
+// Chrometrast
+//
+//
+
+var blackify = '\
+var plist = document.querySelectorAll("p");\
+for (var i = 0; i < plist.length; i++)\
+    plist[i].style.color = "rgba(0, 0, 0, 1.0)";\
+	//plist[i].style.cssText = "color:rgba(0, 0, 0, 1.0) !important";\
+'
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({
+    code: blackify
+  });
+});
