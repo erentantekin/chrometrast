f99c4a2f5b6baab96a34cb74462db63609bc2dc8
Eren Tantekin <eren.tantekin@gmail.com>
2017-05-03 12:53:11 +0300

    fixed unblackifying

--- unblackify.js
+++ unblackify.js
@@ -1,6 +1,4 @@
 var plist = document.querySelectorAll("p, article");
-for (var i = 0; i < plist.length; i++)
-	//plist[i].style.color = "inherit";
+for (var i = 0; i < plist.length; i++) {
 	plist[i].style.color = "";
-	//plist[i].style.cssText = "color:inherit";
-	//plist[i].style.cssText = "color:rgba(0, 0, 0, 1.0) !important";
+}
